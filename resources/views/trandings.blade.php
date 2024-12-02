3a5147a56447378eb429d64ca411099cbf9a8169
Ergia <148410015+Ergiazz@users.noreply.github.com>
2024-12-02 13:39:10 +0700

    Update trandings.blade.php

--- resources/views/trandings.blade.php
+++ resources/views/trandings.blade.php
@@ -49,6 +49,43 @@
         @endif
         
     </main>   
+        <script>
+            
+
+
+
+//geser
+let currentIndex = 0;
+
+
+
+function showSlide(index) {
+  const slider = document.getElementById("slider");
+  const slides = slider.children;
+  const totalSlides = slides.length;
+  
+  if (index >= totalSlides){
+    currentIndex = 0;
+
+  } else if (index < 0) {
+    currentIndex = totalSlides - 1
+  };
+  
+  
+  slider.style.transform = `translateX(-${currentIndex * 272}px)`;
+}
+
+function nextSlide() { 
+
+  currentIndex++;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex--;
+  showSlide(currentIndex);
+}
+        </script>
     <script src="{{ asset('js/trending.js') }}"></script>
     
 </x-layout>
