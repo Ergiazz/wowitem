d1a8318257acf92efa102de099816497e5916830
Ergia <148410015+Ergiazz@users.noreply.github.com>
2024-12-02 13:37:52 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -194,5 +194,30 @@
           
 
     </main>
-<script src="{{ asset('js/dashboard.js') }}"></script>
+//<script src="{{ asset('js/dashboard.js') }}"></script>
+
+<script>
+    let currentIndex = 0;
+
+function showSlide(index) {
+  const slider = document.getElementById("slider");
+  const slides = slider.children;
+  const totalSlides = slides.length;
+  
+  if (index >= totalSlides) currentIndex = 0;
+  if (index < 0) currentIndex = totalSlides - 1;
+  
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex++;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex--;
+  showSlide(currentIndex);
+}
+</script>
 </x-layout.sidelay>
