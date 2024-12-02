9649ce7eb944e9cff15f356ae68b1488f52868f0
Ergia <148410015+Ergiazz@users.noreply.github.com>
2024-12-02 13:36:03 +0700

    Update sidelay.blade.php

--- resources/views/components/layout/sidelay.blade.php
+++ resources/views/components/layout/sidelay.blade.php
@@ -9,6 +9,24 @@
 
     </main>
 
-    <script src="{{ asset('js/navbar.js') }}"></script>
-    <script src="{{ asset('js/items.js') }}"></script>
+    <script>
+        let sidebar = document.getElementById("aside")
+        let hamburger = document.getElementById("hamburger")
+        
+        
+        function muncul(){
+            sidebar.classList.toggle("hidden");
+            sidebar.classList.toggle("fixed");
+            hamburger.classList.toggle("bg-slate-200");
+        }
+        
+        let seacrh = document.getElementById("search")
+        
+        function munculsearch(){
+            search.classList.toggle("hidden");
+        }
+    </script>
+
+    //<script src="{{ asset('js/navbar.js') }}"></script>
+    //<script src="{{ asset('js/items.js') }}"></script>
 </x-layout.layouthtml>
